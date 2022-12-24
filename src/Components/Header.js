770ba17b4e3a14689b74eae3080354b74d875e68
Neil Kardan <neilkardan@Neils-MacBook-Pro.local>
2022-12-23 20:28:27 -0800

    Created home banner and text

---
+++ src/Components/Header.js
@@ -0,0 +1,30 @@
+import './Header.css'
+import React from 'react';
+import Box from '@mui/material/Box';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Toolbar';
+
+const Header = () => {
+    return (
+        <React.Fragment>
+            <Box >
+                <AppBar elevation={0} position="sticky" sx={{bgcolor: '#fcfffc', color: '#121212'}}>
+                    <Toolbar>
+                        <Typography variant='h3' component='h3'>
+                            home
+                        </Typography>
+                        <Typography variant='h3' component='h3'>
+                            projects
+                        </Typography>
+                        <Typography variant='h3' component='h3'>
+                            contact
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+        </React.Fragment>
+    )
+}
+
+export default Header;
---
+++ src/Components/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className="Home">
+            <div className="content-divided">
+                <div className="text-box">
+                    <h3> Hi, I'm Neil Kardan <br/>
+                    I'm a 3rd year Computer Science <br/>
+                    student at UCLA.</h3>
+                </div>
+                <div className="image-hero">
+                    <div className="gradient-box"/>
+                    <div className="image-box"/>   
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
