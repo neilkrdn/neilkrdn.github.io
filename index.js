de8d4f768e81eb6eba1688ba72e66bb5c8650130
Neil Kardan <nkkardan@amazon.com>
2023-07-23 18:26:48 -0700

    moving index

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './src/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
-import "./fb_db.js"
+import "./src/fb_db.js"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
